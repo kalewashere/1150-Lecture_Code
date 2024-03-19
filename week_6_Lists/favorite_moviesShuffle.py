a0a113ac25ddbb7134e2ffced3f8b495d068cdff
kalewashere <mclaughlin.k.taylor@gmail.com>
2024-03-19 14:10:55 -0500

    3/19/24 update - ch 6

--- week_6_Lists/favorite_moviesShuffle.py
+++ week_6_Lists/favorite_moviesShuffle.py
@@ -1,8 +1,11 @@
 import random
+
 movies = ['Amelie', 'The Nightmare Before Christmas', 'Godzilla Vs. Rodan', 'Stand By Me', 'Practical Magic', 'Pink '
                                                                                                               'Flamingoes']
-random.shuffle(movies)
 print(movies)
 
+random.shuffle(movies)
+
+
 for index, movie in enumerate(movies):
-    print(f'{index+1}: {movie}')
+    print(f'{index + 1}: {movie}')
